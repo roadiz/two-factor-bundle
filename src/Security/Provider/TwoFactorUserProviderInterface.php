a048effae946ac60d1f15bc1571c576ce4ae2a0f
roadiz-ci <ambroise+ci@roadiz.io>
2025-06-11 17:46:34 +0000

    chore: Bumped

--- src/Security/Provider/TwoFactorUserProviderInterface.php
+++ src/Security/Provider/TwoFactorUserProviderInterface.php
@@ -6,6 +6,7 @@ namespace RZ\Roadiz\TwoFactorBundle\Security\Provider;
 
 use RZ\Roadiz\CoreBundle\Entity\User;
 use RZ\Roadiz\TwoFactorBundle\Entity\TwoFactorUser;
+use Scheb\TwoFactorBundle\Model\BackupCodeInterface;
 
 interface TwoFactorUserProviderInterface
 {
