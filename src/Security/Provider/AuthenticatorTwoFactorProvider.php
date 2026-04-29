c89c904383c5e416ba2b2f52c3310bc33e107a99
roadiz-ci <ambroise+ci@roadiz.io>
2026-04-29 08:07:57 +0000

    Merge branch hotfix/v2.7.19

--- src/Security/Provider/AuthenticatorTwoFactorProvider.php
+++ src/Security/Provider/AuthenticatorTwoFactorProvider.php
@@ -13,19 +13,20 @@ use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Totp\TotpAuthenticatorInte
 use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\TwoFactorFormRendererInterface;
 use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\TwoFactorProviderInterface;
 
-final class AuthenticatorTwoFactorProvider implements TwoFactorProviderInterface
+final readonly class AuthenticatorTwoFactorProvider implements TwoFactorProviderInterface
 {
     public function __construct(
-        private readonly TwoFactorUserProviderInterface $twoFactorUserProvider,
-        private readonly TotpAuthenticatorInterface $authenticator,
-        private readonly TwoFactorFormRendererInterface $formRenderer,
+        private TwoFactorUserProviderInterface $twoFactorUserProvider,
+        private TotpAuthenticatorInterface $authenticator,
+        private TwoFactorFormRendererInterface $formRenderer,
     ) {
     }
 
+    #[\Override]
     public function beginAuthentication(AuthenticationContextInterface $context): bool
     {
         $user = $context->getUser();
-        if (!($user instanceof User)) {
+        if (!$user instanceof User) {
             return false;
         }
 
@@ -37,38 +38,37 @@ final class AuthenticatorTwoFactorProvider implements TwoFactorProviderInterface
 
         $totpConfiguration = $twoFactorUser->getTotpAuthenticationConfiguration();
         if (null === $totpConfiguration) {
-            throw new TwoFactorProviderLogicException(
-                'User has to provide a TotpAuthenticationConfiguration for TOTP authentication.'
-            );
+            throw new TwoFactorProviderLogicException('User has to provide a TotpAuthenticationConfiguration for TOTP authentication.');
         }
 
         $secret = $totpConfiguration->getSecret();
         if (0 === \mb_strlen($secret)) {
-            throw new TwoFactorProviderLogicException(
-                'User has to provide a secret code for TOTP authentication.'
-            );
+            throw new TwoFactorProviderLogicException('User has to provide a secret code for TOTP authentication.');
         }
 
         return true;
     }
 
+    #[\Override]
     public function prepareAuthentication(object $user): void
     {
     }
 
+    #[\Override]
     public function validateAuthenticationCode(object $user, string $authenticationCode): bool
     {
         if ($user instanceof User) {
             $user = $this->getTwoFactorFromUser($user);
         }
 
-        if (!($user instanceof TwoFactorInterface)) {
+        if (!$user instanceof TwoFactorInterface) {
             return false;
         }
 
         return $this->authenticator->checkCode($user, $authenticationCode);
     }
 
+    #[\Override]
     public function getFormRenderer(): TwoFactorFormRendererInterface
     {
         return $this->formRenderer;
